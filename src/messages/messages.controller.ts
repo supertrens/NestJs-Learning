3d2857ef7020c42b8d7550c50ea555ef75ba60aa
pitrens <pitrens.studio@gmail.com>
2022-03-13 00:12:42 -0500

    code: add function decorators

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'list';
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    return body;
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return 'getting one' + id;
+  }
+}
