46040e72304e710624da76b4b2ddb4afc263f406
pitrens <pitrens.studio@gmail.com>
2022-03-13 00:21:39 -0500

    code: add DTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+
+  // pipe to validate routes
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
