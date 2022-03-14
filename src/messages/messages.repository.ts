4cbc3f679ff87a0c1caa5692bd35f2601fdeab3b
pitrens <pitrens.studio@gmail.com>
2022-03-13 22:09:57 -0400

    code: add dependency injection to the message module

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { readFile, writeFile } from 'fs/promises';
 
 const FILE_NAME = 'messages.json';
 
+@Injectable()
 export class MessagesRepository {
   async findOne(id: string) {
     const messages = await this.parseMessages();
